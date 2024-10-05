231068da7fd2582108dc34c4e77eccd2b1b17dd2
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2024-10-05 14:56:49 -0300

    Add files via upload
    Não subi a pasta (node_modules)

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./component/Header";
+import Banner from "./component/Banner";
+import Container from "./component/Carossel";
+import Card from "./component/Card";
+import Footer from "./component/Footer";
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+      <Banner/>
+      <Container/>
+      <Card/>
+    <Footer/>
+    </>
+  );
+}
+export default App;
---
+++ src/component/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h2>VitorMovies &copy; Desenvolvido por Vitor William 2024</h2>
+        </footer>
+    );
+}
+
+export default Footer;
