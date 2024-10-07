2117facdd4e99f184810943e3700ece05dcc8989
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2024-10-07 20:51:54 -0300

    Add files via upload

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,62 +1,28 @@
 import React from 'react';
-import "../Header/Header.css";
-import appleLogo from '../../Images/icons/apple.png';
-import searchIcon from '../../Images/icons/search.png';
-import bagIcon from '../../Images/icons/bag.png';
+import styles from "../Header/Header.module.css";
 
-function Header() {
-    return (
-        <header className="container2">
+
+function Header(){
+    return(
+        <header className={styles.header}>
+
+            <div className={styles.logo}>
+            <img src='\Images\icons\apple.png'></img>
+            </div>
             <nav>
-                <ul className="nav-list nav-list-larger">
-                    <li className="nav-item search-hiden">
-                        <input
-                            className="nav-link nav-link-searchbar"
-                            type="text"
-                            placeholder="&#xF002; Search apple.com"
-                            style={{ fontFamily: "Arial, FontAwesome" }}
-                        />
-                    </li>
-                    <li className="nav-item">
-                        <a href="/" className="nav-link">
-                            <img src={appleLogo} alt="Apple Logo" className="nav-link-apple" />
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="mac" className="nav-link">Mac</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">iPad</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="iphone" className="nav-link">iPhone</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="watch" className="nav-link">Watch</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">TV</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Music</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Support</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">
-                            <img src={searchIcon} alt="Search Icon" className="nav-link-search" />
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">
-                            <img src={bagIcon} alt="Bag Icon" className="nav-link-bag" />
-                        </a>
-                    </li>
-                </ul>
+            <a href="#">Mac</a>
+            <a href="#">iPad</a>
+            <a href="#">iPhone</a>
+            <a href="#">Watch</a>
+            <a href="#">TV</a>
+            <a href="#">Music</a>
+            <a href="#">Support</a>
             </nav>
+            <div className={styles.icons}>
+            <img src='\Images\icons\search.png'></img>
+            <img src='\Images\icons\bag.png'></img>
+            </div>
         </header>
-    );
+    )
 }
-
 export default Header;
