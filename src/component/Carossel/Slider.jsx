e888cbfd9093c92d4bbeb975007a935099bbf785
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2024-10-07 22:21:33 -0300

    Update Slider.jsx

--- src/component/Carossel/Slider.jsx
+++ src/component/Carossel/Slider.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import "../Carossel/Carossel.module.css"
+import React from 'react';
+import "../Carossel/Carossel.module.css";
 import Carousel from "react-multi-carousel";
-
+import "react-multi-carousel/lib/styles.css";
 
 const sliderImageUrl = [
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16BRANCO.jpg"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16BRANCO.jpg"
   },
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16CORES.JPG"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16CORES.JPG"
   },
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16PRETO.jpg"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16PRETO.jpg"
   },
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16ROSA.jpg"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16ROSA.jpg"
   },
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16ULTRAMARINO.jpg"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16ULTRAMARINO.jpg"
   },
   {
-    url:
-      "https://storageimagepi.blob.core.windows.net/images/IPHONE16VERDEACIZENTADO.jpg"
+    url: "https://storageimagepi.blob.core.windows.net/images/IPHONE16VERDEACIZENTADO.jpg"
   },
 ];
 
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
 const Slider = () => {
   return (
-    <div className="parent">
+    <div className="parent" style={{ marginTop: '70px' }}>
       <Carousel
+        responsive={responsive}
         autoPlay={true}
         swipeable={true}
         draggable={true}
@@ -44,8 +58,8 @@ const Slider = () => {
       >
         {sliderImageUrl.map((imageUrl, index) => {
           return (
-            <div className="slider" key={index}>
-              <img src={imageUrl.url} alt="product" />
+            <div className="slider" key={index} style={{ display: 'flex', justifyContent: 'center' }}>
+              <img src={imageUrl.url} alt="product" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
             </div>
           );
         })}
@@ -53,4 +67,5 @@ const Slider = () => {
     </div>
   );
 };
+
 export default Slider;
